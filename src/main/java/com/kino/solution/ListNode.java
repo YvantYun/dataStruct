c949731b666045e25fcc7f62e7656be576bf5645
kino <374932573@qq.com>
2019-02-04 15:52:20 +0800

    leetcode

---
+++ src/main/java/com/kino/solution/ListNode.java
@@ -0,0 +1,16 @@
+package com.kino.solution;
+
+/**
+ * @Author: kino
+ * @Date: 2019/2/4 10:54
+ */
+public class ListNode {
+
+    int val;
+
+    ListNode next;
+
+    ListNode(int x) { val = x; }
+
+
+}
